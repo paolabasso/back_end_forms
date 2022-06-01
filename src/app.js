326626a6feb7683dd8baef35feef15f26fff15bf
Paola Basso <paolasbasso@gmail.com>
2022-06-01 09:02:47 -0300

    implementação cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import router from "./routes.js";
 import { connectionDB } from "./database/mongo.js";
+
 
 dotenv.config();
 const app = express();
 connectionDB();
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 //essa função transformará o que vem do cliente em JSON
 app.use(express.json());
